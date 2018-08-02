2146f5194ef537b378260507fe4e3c8c6481647f
Dan <ccastro.danilo@gmail.com>
2018-08-01 22:54:28 -0300

    bug fix

--- src/fetch-injection.ts
+++ src/fetch-injection.ts
@@ -1,16 +1,25 @@
+import {win} from "window-var";
+// start global vars ------------
+if ((win as any).vis === undefined) {
+  (win as any).vis = {};
+}
+((win as any).vis as any).fetchAction = (url: string) => {
+  console.warn(`could not load the ${url} because you didn't register the fetch action.`);
+};
+// end global ----------------
 export type FetchAction = (url: string) => void;
 export namespace FetchInjection {
   export interface IFetchCallback {
     onSuccess: (url: string) => void;
     onError: (url: string) => void;
   }
-  let fetchAction: FetchAction = (url: string) => {
-    console.warn(`could not load the ${url} because you didn't register the fetch action.`);
-  };
+  // let fetchAction: FetchAction = (url: string) => {
+  //   console.warn(`could not load the ${url} because you didn't register the fetch action.`);
+  // };
   export function inject(action: FetchAction) {
-    fetchAction = action;
+    ((win as any).vis as any).fetchAction = action;
   }
   export function getFetchAction(): FetchAction {
-    return fetchAction;
+    return ((win as any).vis as any).fetchAction;
   }
 }
--- src/instance-registration-injection.ts
+++ src/instance-registration-injection.ts
@@ -1,14 +1,25 @@
+import {win} from "window-var";
+// start global vars ------------
+if ((win as any).vis === undefined) {
+  (win as any).vis = {};
+}
+((win as any).vis as any).registrationRootStore = <T>(stateName: string, stateRootInstance: T) => {
+  console.error("You didn't inject the 'RegistrationRootStore' function.");
+  return stateRootInstance;
+};
+// end global ----------------
+
 export type InstanceRegistrationAction = <T>(stateName: string, stateRootInstance: T) => T;
 export namespace InstanceRegistrationInjection {
-  let RegistrationRootStore: InstanceRegistrationAction = <T>(stateName: string, stateRootInstance: T) => {
-    console.error("You didn't inject the 'RegistrationRootStore' function.");
-    return stateRootInstance;
-  };
+  // let RegistrationRootStore: InstanceRegistrationAction = <T>(stateName: string, stateRootInstance: T) => {
+  //   console.error("You didn't inject the 'RegistrationRootStore' function.");
+  //   return stateRootInstance;
+  // };
   export function inject(RegistrationRootStoreFunction: InstanceRegistrationAction) {
-    RegistrationRootStore = RegistrationRootStoreFunction;
+    ((win as any).vis as any).registrationRootStore = RegistrationRootStoreFunction;
   }
   export function getInstanceRegistrationAction(): InstanceRegistrationAction {
-    return RegistrationRootStore;
+    return ((win as any).vis as any).registrationRootStore;
   }
   
 }
